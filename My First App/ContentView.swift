ba67d9b8679268b7c26273287cdf3b48af007d18
Leona Chen <leonachenpiano@gmail.com>
2023-07-28 13:32:18 -0400

    changed sentence

--- My First App/ContentView.swift
+++ My First App/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Here's a cute puppy!")
+            Text("Very nice puppy!")
                 .font(.largeTitle)
                 .foregroundColor(Color.purple)
             Image("puppy")
